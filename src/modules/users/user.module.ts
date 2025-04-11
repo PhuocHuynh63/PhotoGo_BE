4f7c8f94cb1888a755f073c7d84427e2ea1c8363
cc <thuanhdse171787@fpt.edu.vn>
2025-04-12 01:39:34 +0700

    feat(users): add upload and mail modules to user module imports

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -5,11 +5,15 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User } from './entities/user.entity';
 import { RoleModule } from '../roles/role.module';
+import { UploadModule } from 'src/3rdService/upload/upload.module';
+import { MailModule } from 'src/3rdService/mail/mail.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     RoleModule, 
+    UploadModule,
+    MailModule,
   ],
   providers: [UserService],
   controllers: [UserController],
