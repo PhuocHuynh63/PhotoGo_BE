f040667f59a47d57b887fa416d3ea70084c8e113
Huỳnh Minh Phước <118465492+PhuocHuynh63@users.noreply.github.com>
2025-07-12 01:00:16 +0700

    Enhance Gemini module and service to include ServiceConcept and ServiceConceptImage entities, improving concept analysis with additional details such as name, price, and image URL.

--- src/3rdService/gemini/gemini.module.ts
+++ src/3rdService/gemini/gemini.module.ts
@@ -4,12 +4,15 @@ import { GeminiService } from './gemini.service';
 import { GeminiController } from './gemini.controller';
 import geminiConfig from './config/gemini.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ConceptVector } from '../../modules/service-package/entities/concept-vector.entity';
+import { ServiceConcept } from '../../modules/service-package/entities/service-concept.entity';
+import { ServiceConceptImage } from '../../modules/service-package/entities/service-concept-image.entity';
 
 @Module({
     imports: [
         ConfigModule.forFeature(geminiConfig),
-        TypeOrmModule.forFeature([ConceptVector]),
+        TypeOrmModule.forFeature([ConceptVector, ServiceConcept, ServiceConceptImage]),
     ],
     controllers: [GeminiController],
     providers: [GeminiService],
