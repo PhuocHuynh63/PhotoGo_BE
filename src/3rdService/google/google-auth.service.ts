3c0ab8fcd821a265f1200c7e5238458a436b3424
PhuocHuynh63 <phuochmse171830@fpt.edu.vn>
2025-07-25 08:46:19 +0700

    Restore cart and wishlist retrieval in GoogleAuthService to enhance user data response during authentication, including cart and wishlist IDs in the response payload.

--- src/3rdService/google/google-auth.service.ts
+++ src/3rdService/google/google-auth.service.ts
@@ -58,8 +58,8 @@ export class GoogleAuthService {
     }
 
 
-    // const cart = await this.cartService.findCartByUserId(existingUser.id);
-    // const wishlist = await this.wishlistService.findWishlistByUserId(existingUser.id);
+    const cart = await this.cartService.findCartByUserId(existingUser.id);
+    const wishlist = await this.wishlistService.findWishlistByUserId(existingUser.id);
 
 
     // Kiểm tra trạng thái tài khoản
@@ -88,9 +88,9 @@ export class GoogleAuthService {
         email: existingUser.email,
         fullname: existingUser.fullName,
         image: existingUser.avatarUrl,
-        role: existingUser.role,
-        // cartId: cart?.id || null,
-        // wishlistId: wishlist?.id || null,
+        role: rolePayload,
+        cartId: cart?.id || null,
+        wishlistId: wishlist?.id || null,
         subscriptionId: existingUser.subscription?.id,
       }, access_token_jwt: accessToken
     };
